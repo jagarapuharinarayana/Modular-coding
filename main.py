9954938e45e6998fd25bae0c0caa6c70437a2676
jagarapuhari <32777531+jagarapuharinarayana@users.noreply.github.com>
2023-11-16 10:01:30 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# modular coding
+
+from utils import *
+
+print(add(2,5))
+print(sub(20,5))
+
+
+res = mathOperations(2,55)
+print(res.ADD())
+
+
+res1 = otherClass(50,10)
+
+print(res1.SUB())
---
+++ utils.py
@@ -0,0 +1,25 @@
+# u can write any number of functions in this py file
+
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+
+class mathOperations:
+
+    def __init__(self,a,b,):
+        self.a = a
+        self.b = b
+
+
+    def ADD(self):
+        return self.a + self.b
+    
+class otherClass(mathOperations):
+
+    def SUB(self):
+        return self.a - self.b
+    
